84cd3a16292368de65b8adeadca4c917e032b534
Bruno Godinho <54083619+gmbrunoo@users.noreply.github.com>
2022-08-19 17:34:15 -0300

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -11,7 +11,7 @@
   <title>Acessibilidade</title>
 
   <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="./css/bootstrap.min.css" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="./css/style.css" rel="stylesheet">
